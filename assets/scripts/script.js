d519b9dca2c57017e9102d0baf754e597352461a
Arief Hirmanto <ariefhirmanto7@gmail.com>
2020-05-09 11:45:24 +0700

    finishing, added fetch API and responsive layout

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,24 +1,54 @@
 // Get Data from API
 let url = "https://covid19.mathdro.id/api/confirmed";
 let proxyUrl = "https://fierce-fortress-83266.herokuapp.com/";
-let listOfCountry = ["US", "Spain", "Italy", "United Kingdom", "Russia", "France", "Germany", "Brazil", "Turkey", "Iran", "Indonesia"];
+let listOfCountry = ["US", "Spain", "Italy", "France", "Russia", "Germany", "United Kingdom", "Brazil", "Turkey", "Iran", "Indonesia"];
 let objData = {};
+let dataUS = {
+    confirmed: 0,
+    death: 0,
+    recovered: 0
+};
+let dataFR = {
+    confirmed: 0,
+    death: 0,
+    recovered: 0,   
+}
+let dataUK = {
+    confirmed: 0,
+    death: 0,
+    recovered: 0
+}
 
 fetch(proxyUrl + url, { mode: 'cors'})
     .then(res => res.json())
     .then(data => {
         console.log(data);
         for (country in data) {
-            if (listOfCountry.includes(data[country].countryRegion)) {
+            if (data[country].countryRegion === 'US') {
+                dataUS.confirmed += data[country].confirmed;
+                dataUS.death += data[country].deaths;
+                dataUS.recovered += data[country].recovered;
+            } else if (data[country].countryRegion === 'United Kingdom') {
+                dataUK.confirmed += data[country].confirmed;
+                dataUK.death += data[country].deaths;
+                dataUK.recovered += data[country].recovered;
+            } else if (data[country].countryRegion === 'France') {
+                dataFR.confirmed += data[country].confirmed;
+                dataFR.death += data[country].deaths;
+                dataFR.recovered += data[country].recovered;
+            } else if (listOfCountry.includes(data[country].countryRegion)) {
                 let listData = {
                     confirmed: data[country].confirmed,
                     death: data[country].deaths,
                     recovered: data[country].recovered
                 }
                 objData[data[country].countryRegion] = listData;
-            }
-            updateDisplayCountry(objData[data[country].countryRegion]);
+            } 
+            objData['US'] = dataUS;
+            objData['France'] = dataFR;
+            objData['United Kingdom'] = dataUK;
         }
+        updateDisplayCountry();
         updateDisplayIndonesia();
     }).catch(err => {
         console.error('Error: ', err);
@@ -30,14 +60,15 @@ function updateDisplayIndonesia() {
     document.querySelector("#indonesia-death").innerText = objData.Indonesia.death;
 }
 
-function updateDisplayCountry(countries) {
+function updateDisplayCountry() {
     let covidList = document.querySelector("#covidList");
     covidList.innerHTML = "";
-    for (country in )
-    let row = document.createElement('tr');
-    row.innerHTML = "<td>" + country + "</td>";
-    row.innerHTML += "<td>" + country.confirmed + "</td>";
-    row.innerHTML += "<td>" + country.recovered + "</td>";
-    row.innerHTML += "<td>" + country.death + "</td>";
-    covidList.appendChild(row);   
+    for (country of listOfCountry) {
+        let row = document.createElement('tr');
+        row.innerHTML = "<td>" + country + "</td>";
+        row.innerHTML += "<td>" + objData[country].confirmed + "</td>";
+        row.innerHTML += "<td>" + objData[country].recovered + "</td>";
+        row.innerHTML += "<td>" + objData[country].death + "</td>";
+        covidList.appendChild(row);
+    }   
 }
